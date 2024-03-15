67981cabbcfd6f21040b62838fb8ea21288f11ab
maxstein <supremetnf54@gmail.com>
2024-03-15 14:19:43 +0100

    fix register with firstname et lastname

--- src/api/controllers/auth/authController.js
+++ src/api/controllers/auth/authController.js
@@ -2,15 +2,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res, db) => {
-    const { firstname, email, password } = req.body;
+    const { firstname,lastname, email, password } = req.body;
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        db.query('INSERT INTO users (firstname, email, password) VALUES (?, ?, ?)', [firstname, email, hashedPassword], (error, results) => {
+        db.query('INSERT INTO users (firstname,lastname, email, password) VALUES (?, ?, ?)', [firstname,lastname, email, hashedPassword], (error, results) => {
             if (error) {
                 console.error('Erreur lors de la création de l’utilisateur:', error);
                 return res.status(500).send('Erreur lors de la création de l’utilisateur.');
             }
-            res.status(201).send('Utilisateur inscris avec succès.');
+            res.status(201).send('Utilisateur inscrit avec succès.');
         });
     } catch (error) {
         console.error(error);
