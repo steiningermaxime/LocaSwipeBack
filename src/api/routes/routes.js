a04508250493b4363131e597d2e65a2d26a3c3f4
maxstein <supremetnf54@gmail.com>
2024-06-06 11:06:14 +0200

    fix error when user like 2 times the same appointment

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = (db) => {
+module.exports = (db, io) => {
   const userRoutes = require('./users/userRoutes')(db);
   const authRoutes = require('./auth/authRoutes')(db);
-  const accomodationRoutes = require('./accomodation/accomodationRoutes')(db);
+  const accomodationRoutes = require('./accomodation/accomodationRoutes')(db, io);
   const messagingRoutes = require('./messaging/messagingRoutes')(db);
 
   router.use('/users', userRoutes);
   router.use('/auth', authRoutes);
   router.use('/messaging', messagingRoutes);
   router.use('/accommodations', accomodationRoutes);
-
 
   return router;
 };
