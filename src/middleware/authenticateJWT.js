b133292fb2e800d76f1b7104806ca4cb1a4d8228
maxstein <supremetnf54@gmail.com>
2024-06-16 01:59:46 +0200

    auth route wip

--- src/middleware/authenticateJWT.js
+++ src/middleware/authenticateJWT.js
@@ -1,14 +1,20 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userData = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).send({
-            message: 'Authentification échouée'
-        });
-    }
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+  if (!authHeader) return res.status(401).send('Access Denied');
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) return res.status(401).send('Access Denied');
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send('Invalid Token');
+  }
 };
+
+module.exports = authenticateJWT;
